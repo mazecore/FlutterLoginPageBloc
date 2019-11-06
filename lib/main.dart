878651dcf414675da018550af5ab2d9c5d2e0197
Ildar Masagutov <masagutov@yahoo.com>
2019-11-06 00:26:03 -0500

    added gradient background and centered the form

--- lib/main.dart
+++ lib/main.dart
@@ -33,102 +33,126 @@ class _LoginPageState extends State<LoginPage> {
     final bloc = Bloc();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign In"),
-        backgroundColor: Colors.deepPurpleAccent,
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        // color: Colors.deepPurpleAccent,
-        // height: MediaQuery.of(context).size.height,
-        padding: EdgeInsets.all(50),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StreamBuilder<String>(
-              stream: bloc.email,
-              builder: (context, snapshot) => TextFormField(
-                onChanged: bloc.emailChanged,
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.tealAccent),
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
-                    ),
-                    // hintText: "Enter email",
-                    // hintStyle: TextStyle(color: Colors.deepPurpleAccent),
-                    labelText: "Email",
-                    labelStyle: TextStyle(color: Colors.white),
-                    errorText: snapshot.error,
-                    errorStyle: TextStyle(color: Colors.redAccent),
-                    fillColor: Colors.deepPurpleAccent[100],
-                    focusColor: Colors.deepPurple[50],
-                    filled: true),
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            StreamBuilder<String>(
-              stream: bloc.password,
-              builder: (context, snapshot) => TextFormField(
-                onChanged: bloc.passwordChanged,
-                keyboardType: TextInputType.text,
-                style: TextStyle(color: Colors.tealAccent),
-                textAlign: TextAlign.center,
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                      borderSide: BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      ),
-                    ),
-                    // hintText: "Enter password",
-                    // hintStyle: TextStyle(color: Colors.deepPurpleAccent),
-                    labelText: "Password",
-                    labelStyle: TextStyle(color: Colors.white),
-                    errorText: snapshot.error,
-                    errorStyle: TextStyle(color: Colors.redAccent),
-                    fillColor: Colors.deepPurpleAccent[100],
-                    filled: true,
-                    focusColor: Colors.deepPurple[50]),
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            SizedBox(
-              // height: MediaQuery.of(context).size.height,
-              width: 300,
-              // height: 24.0,
-              child: StreamBuilder<bool>(
-                stream: bloc.submitCheck,
-                builder: (context, snapshot) => RaisedButton(
-                    padding: EdgeInsets.all(20),
+      backgroundColor: Colors.deepPurple,
+      // appBar: AppBar(
+      //   title: Text("Sign in with your instagram account"),
+      //   backgroundColor: Colors.deepPurpleAccent,
+      //   centerTitle: true,
+      //   elevation: 0,
+      // ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.deepPurpleAccent, Colors.deepPurple])),
+          padding: EdgeInsets.all(50),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40,
+                  top: 0,
+                  right: 10,
+                  bottom: 80,
+                ),
+                child: Text(
+                  "Sign in with your instagram account",
+                  style: TextStyle(
                     color: Colors.white,
-                    child: Text("LOG IN"),
-                    textColor: Colors.deepPurpleAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40.0),
-                    ),
-                    onPressed:
-                        // snapshot.hasData
-                        () => changeThePage(context)
-                    // ? () => changeThePage(context)
-                    // : changeThePage(context),
-                    ),
+                    fontSize: 20,
+                  ),
+                ),
               ),
-            ),
-          ],
+              StreamBuilder<String>(
+                stream: bloc.email,
+                builder: (context, snapshot) => TextFormField(
+                  onChanged: bloc.emailChanged,
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      hintText: "Email",
+                      hintStyle: TextStyle(color: Colors.deepPurpleAccent),
+                      // labelText: "Email",
+                      // labelStyle: TextStyle(color: Colors.white),
+                      errorText: snapshot.error,
+                      errorStyle: TextStyle(color: Colors.redAccent),
+                      fillColor: Colors.deepPurpleAccent[100],
+                      focusColor: Colors.deepPurple[50],
+                      filled: true),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              StreamBuilder<String>(
+                stream: bloc.password,
+                builder: (context, snapshot) => TextFormField(
+                  onChanged: bloc.passwordChanged,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Colors.deepPurpleAccent),
+                      // labelText: "Password",
+                      // labelStyle: TextStyle(color: Colors.white),
+                      errorText: snapshot.error,
+                      errorStyle: TextStyle(color: Colors.redAccent),
+                      fillColor: Colors.deepPurpleAccent[100],
+                      filled: true,
+                      focusColor: Colors.deepPurple[50]),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  SizedBox(
+                    width: 100,
+                    child: StreamBuilder<bool>(
+                      stream: bloc.submitCheck,
+                      builder: (context, snapshot) => RaisedButton(
+                          padding: EdgeInsets.all(20),
+                          color: Colors.white,
+                          child: Text("LOG IN"),
+                          textColor: Colors.deepPurpleAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                          ),
+                          onPressed:
+                              // snapshot.hasData
+                              () => changeThePage(context)
+                          // ? () => changeThePage(context)
+                          // : changeThePage(context),
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
